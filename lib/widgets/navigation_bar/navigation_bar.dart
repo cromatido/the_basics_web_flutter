b40704d1661dd1e87e77862d5bd442676e52474a
Williamwr <williamwr@gmail.com>
2020-02-13 21:33:46 -0400

    Initial commit

---
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget {
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+            width: 150,
+            child: Image.asset('assets/logo.png',width: 600,height: 240,fit:BoxFit.cover),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              _NavBarItem('Episodes'),
+              SizedBox(
+                width: 60,
+              ),
+              _NavBarItem('About')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _NavBarItem extends StatelessWidget {
+  final String title;
+  const _NavBarItem(this.title, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 18),
+    );
+  }
+}
